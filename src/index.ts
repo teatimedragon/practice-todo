a290d5e5086cb7e5af1eb346c70e66dfdfe60bbe
teatimedragon <1159@hey.com>
2026-07-09 13:05:42 -0700

    final todo results

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import { TodoItem } from "./todoItem.js";
 import { TodoCollection } from "./todoCollection.js";
 
-import { input, rawlist } from "@inquirer/prompts"
+import { input, rawlist, checkbox } from "@inquirer/prompts"
+import { JsonTodoCollection } from "./jsonTodoCollection.js";
 
-let todos = [
+let todos: TodoItem[] = [
     new TodoItem(1, "Buy flowers"), new TodoItem(2, "Get shoes"),
     new TodoItem(3, "Collect stuff"), new TodoItem(4, "Call Rob", true)
 ]
 
-let collection = new TodoCollection("Peter", todos)
+let collection: TodoCollection = new JsonTodoCollection("Peter", todos)
 let showCompleted = true
 
 function displayTodoList(): void {
@@ -19,7 +20,9 @@ function displayTodoList(): void {
 
 enum Commands {
     Add = "Add New Task",
+    Complete = "Complete a Task",
     Toggle = "Show/Hide Completed",
+    Purge = "Remove Completed Tasks",
     Quit = "Quit"
 }
 
@@ -37,6 +40,22 @@ function promptAdd(): void {
     )
 }
 
+function promptComplete(): void {
+    console.clear()
+    checkbox({
+        message: "Mark Tasks Complete",
+        choices: collection.getTodoItems(showCompleted).map(
+            item => (
+                { name: item.task, value: item.id, checked: item.complete }
+            ))
+    }).then(answers => {
+        collection.getTodoItems(true).forEach(
+            item => collection.markComplete(item.id, answers.find(id => id === item.id) != undefined)
+        )
+        promptUser()
+    })
+}
+
 function promptUser(): void {
     console.clear()
     displayTodoList()
@@ -51,6 +70,17 @@ function promptUser(): void {
                 break
             case Commands.Add:
                 promptAdd()
+                break
+            case Commands.Complete:
+                if (collection.getItemCounts().incomplete > 0) {
+                    promptComplete()
+                } else {
+                    promptUser()
+                }
+                break
+            case Commands.Purge:
+                collection.removeComplete()
+                promptUser()
                 break
         }
     })
